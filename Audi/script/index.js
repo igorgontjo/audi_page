30ae1253d48a3df202dc7033aaf1342ff68ec579
Igor Marques <98192277+igorgontjo@users.noreply.github.com>
2022-03-03 14:13:15 -0300

    Add files via upload

---
+++ Audi/script/index.js
@@ -0,0 +1,14 @@
+// Bootstrap
+src = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+integrity = "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+crossorigin = "anonymous"
+
+$(function(){
+
+  $('.boxCar').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+  });
+})
